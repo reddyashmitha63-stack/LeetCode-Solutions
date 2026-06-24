c1e04cf0b241affc28d4a185f2b1b095e6169ab2
Varla Ashmitha Reddy <reddyashmitha63@gmail.com>
2026-06-24 16:16:25 +0530

    Time: 0 ms (100%), Space: 13.4 MB (35.04%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        size=len(nums)
+        low=0
+        high=size-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                high=mid-1
+            elif nums[mid]<target:
+                low=mid+1
+        return -1
+        
+
+
+        
