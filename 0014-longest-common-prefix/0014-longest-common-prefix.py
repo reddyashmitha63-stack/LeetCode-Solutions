9eb70b7be1d37a13149f01471b085d845a70d230
Varla Ashmitha Reddy <reddyashmitha63@gmail.com>
2026-07-02 17:46:03 +0530

    Time: 0 ms (100%), Space: 12.5 MB (2.98%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        strs.sort()
+        first=strs[0]
+        last=strs[-1]
+        ans=[]
+        for i in range(min(len(first),len(last))):
+            if first[i]!=last[i]:
+                return ''.join(ans)
+            ans.append(first[i])
+        return ''.join(ans)
+
+        
